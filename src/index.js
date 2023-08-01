80922657683b303cf928fcd747ac0d7de8f82fc7
vulpies <anonim616@gmail.com>
2023-08-02 01:46:43 +0300

    tests done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+	let result
+	const str = n.toString().split("").reverse()
+
+	if (str[str.length - 1] === '-') {
+		return result = str.slice(0, str.length - 1).join('')
+	} else {
+		return result = str.join('')
+	}
 }
